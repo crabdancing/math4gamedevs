d4271a8aee14d60352f4ff3d276bd4f6c4c05e0e
Alexandria P <alxpettit@gmail.com>
2022-07-05 15:12:42 -0700

    ok dot product time

--- 02_motion_between_dots/Node2D.gd
+++ 02_motion_between_dots/Node2D.gd
@@ -14,14 +14,17 @@ func _draw():
 	var posA = $posA.position
 	var posB = $posB.position
 	if posA != null && posB != null:
-		var a_to_b_trajectory = (posB - posA).normalized()
+		var a_to_b_trajectory = posB - posA
 		draw_line(posA, posB, Color(0.683594, 0.683594, 0.683594) , true)
 		draw_circle(posA, 10, Color(0.042969, 0.282227, 1))
 		draw_circle(posB, 10, Color(0.996094, 0.513611, 0))
-		var node_along_path = a_to_b_trajectory * path_along_trajectory + posA
-		
-		draw_circle(node_along_path,
-			10, Color(0.960938, 0.22522, 0.483871))
+		var node_along_path = a_to_b_trajectory.normalized() * path_along_trajectory
+		# Unnecessary but OCD requires this check, I guess -- IDK
+		if node_along_path.length() <= a_to_b_trajectory.length() \
+			and path_along_trajectory >= 0:
+
+			draw_circle(node_along_path + posA,
+				10, Color(0.960938, 0.22522, 0.483871))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
