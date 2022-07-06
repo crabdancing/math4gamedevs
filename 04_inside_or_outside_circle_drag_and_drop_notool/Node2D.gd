f545136f96c8b937565da9b28fabf2a7c98e2529
Alexandria P <alxpettit@gmail.com>
2022-07-05 19:28:24 -0700

    sdijdsjsdiojsdoijsdoijasoiasjioasjiopasj

--- 04_inside_or_outside_circle_drag_and_drop_notool/Node2D.gd
+++ 04_inside_or_outside_circle_drag_and_drop_notool/Node2D.gd
@@ -10,17 +10,19 @@ onready var old_button_pressed = false
 
 func _input(ev):
 	if ev is InputEventMouseButton:
-		old_button_pressed = button_pressed
 		button_pressed = ev.pressed
 	elif ev is InputEventMouseMotion:
-		if button_pressed and !old_button_pressed:
-			var pos = ev.get_position() - get_canvas_transform().get_origin()
-			if $TestPoint.is_within(pos):
-				$TestPoint.is_being_dragged = true
-		else:
-			$TestPoint.is_being_dragged = false			
 		if $TestPoint.is_being_dragged:
 			$TestPoint.translate(ev.relative)
+		if button_pressed:
+			if !old_button_pressed:
+				var pos = ev.get_position() - get_canvas_transform().get_origin()
+				if $TestPoint.is_within(pos):
+					$TestPoint.is_being_dragged = true
+		else:
+			$TestPoint.is_being_dragged = false
+			
+		old_button_pressed = button_pressed
 	# You could calculate with `distance` vs `radius`, but cutting off part of the pythagorean theorem
 	# saves processing power by only making the op necessary if the circle radius changes,
 	# and replacing sqrt with mult op.
