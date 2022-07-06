550269b40df328753bb493d034a2bb020549958b
Alexandria P <alxpettit@gmail.com>
2022-07-05 19:12:18 -0700

    Was supposed to be walking but I thought of something cool to try bleh I am a bad girl Y~Y

---
+++ 04_inside_or_outside_circle_drag_and_drop_notool/Node2D.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+onready var button_pressed = false
+onready var old_button_pressed = false
+
+func _input(ev):
+	if ev is InputEventMouseButton:
+		old_button_pressed = button_pressed
+		button_pressed = ev.pressed
+	elif ev is InputEventMouseMotion:
+		if button_pressed and !old_button_pressed:
+			var pos = ev.get_position() - get_canvas_transform().get_origin()
+			if $TestPoint.is_within(pos):
+				$TestPoint.is_being_dragged = true
+		else:
+			$TestPoint.is_being_dragged = false			
+		if $TestPoint.is_being_dragged:
+			$TestPoint.translate(ev.relative)
+	# You could calculate with `distance` vs `radius`, but cutting off part of the pythagorean theorem
+	# saves processing power by only making the op necessary if the circle radius changes,
+	# and replacing sqrt with mult op.
+	$TestPoint.is_inside = $TestPoint.position.distance_squared_to($Circle.position) < $Circle.radius_squared
+
---
+++ 04_inside_or_outside_circle_drag_and_drop_notool/TestPoint.gd
@@ -0,0 +1,25 @@
+tool
+extends Position2D
+
+onready var is_inside = false
+onready var color = Color(0, 0, 0)
+onready var is_being_dragged = false
+export var radius = 10
+var radius_sq = radius*radius
+func _draw():
+	if is_inside == true:
+		color = Color(0, 255, 0)
+	elif is_inside == false:
+		color = Color(255, 0, 0)
+	else:
+		color = Color(0, 0, 0)
+	draw_circle(position - transform.get_origin(), radius, color)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	radius_sq = radius * radius
+	update()
+#	pass
+
+func is_within(v: Vector2) -> bool:
+	return position.distance_squared_to(v) <= radius_sq
